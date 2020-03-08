b1e22aba2416b5084db7cb71b68b56a6543b737b
NaveenK-1191 <karnamsnaveen@gmail.com>
2020-03-08 10:07:07 +0530

    Regression work going on

--- src/test/java/maven/BrowserTest.java
+++ src/test/java/maven/BrowserTest.java
@@ -7,6 +7,10 @@ public class BrowserTest {
 public void test1() {
 	System.out.println("testing");
 	System.out.println("testing1");
+	System.out.println("testing2");
+	System.out.println("testing3");
+	System.out.println("testing4");
+	
 	
 }
 }
